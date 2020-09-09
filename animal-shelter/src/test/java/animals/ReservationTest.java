658bd5e3bb9fa9bc606b62450c74b81b0dd77871
Nick Thijssen <nickthijssen1994@gmail.com>
2020-09-09 15:39:37 +0200

    Completed C# To Java

--- animal-shelter/src/test/java/animals/ReservationTest.java
+++ animal-shelter/src/test/java/animals/ReservationTest.java
@@ -1,7 +1,24 @@
 package animals;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ReservationTest {
 
+    private final Reservation reservation = new Reservation();
+
+    @Test
+    public void TestNewCat(){
+        assertEquals(0, this.reservation.getAnimals().size());
+        this.reservation.newCat("Ms. Meow", Gender.FEMALE, "Scratches Couch");
+        assertEquals(1, this.reservation.getAnimals().size());
+    }
+
+    @Test
+    public void TestNewDog(){
+        assertEquals(0, this.reservation.getAnimals().size());
+        this.reservation.newDog("Sgt. Woof", Gender.MALE);
+        assertEquals(1, this.reservation.getAnimals().size());
+    }
 }
--- animal-shelter/src/test/java/animals/ReservorTest.java
+++ animal-shelter/src/test/java/animals/ReservorTest.java
@@ -1,7 +1,18 @@
 package animals;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ReservorTest {
 
+    @Test
+    public void TestConstructor(){
+        Date reservedAt = new Date();
+        Reservor reservor = new Reservor("John Doe", reservedAt);
+        assertEquals("John Doe", reservor.getName());
+        assertEquals(reservedAt, reservor.getReservedAt());
+    }
 }
