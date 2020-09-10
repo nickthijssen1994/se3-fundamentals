17f21c7c30adfad7e17c1f7a9b7a1cb7ec7daac3
Nick Thijssen <nickthijssen1994@gmail.com>
2020-09-10 17:05:14 +0200

    Added Animal Shelter GUI

--- animal-shelter/src/main/java/gui/Controller.java
+++ animal-shelter/src/main/java/gui/Controller.java
@@ -1,9 +1,74 @@
 package gui;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
 import webshop.Webshop;
 
 public class Controller {
+
+    @FXML
+    private GridPane grid;
+
+    @FXML
+    private TextField tfAnimalName;
+
+    @FXML
+    private ChoiceBox<?> cbAnimalGender;
+
+    @FXML
+    private RadioButton rbCat;
+
+    @FXML
+    private RadioButton rbDog;
+
+    @FXML
+    private DatePicker dpLastWalk;
+
+    @FXML
+    private TextField tfBadHabbit;
+
+    @FXML
+    private Button btAddAnimal;
+
+    @FXML
+    private TextField tfProductName;
+
+    @FXML
+    private Spinner<?> spProductPrice;
+
+    @FXML
+    private Button btAddProduct;
+
+    @FXML
+    private TextField tfUserName;
+
+    @FXML
+    private Button btReserveAnimal;
+
+    @FXML
+    private Button btWalkDog;
+
+    @FXML
+    private Button btAddToCart;
+
+    @FXML
+    private Button btRemoveFromCart;
+
+    @FXML
+    private ListView<?> lvAnimals;
+
+    @FXML
+    private ListView<?> lvProducts;
+
+    @FXML
+    private ListView<?> lvCart;
+
+    @FXML
+    private TextField tfTotalPrice;
+
+    @FXML
+    private Button btCheckout;
 
     private Webshop webshop;
 
