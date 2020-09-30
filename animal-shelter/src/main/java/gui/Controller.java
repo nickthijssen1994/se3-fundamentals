2792790d138cd5995f09dfa1c86fe44eed4a65c0
Nick Thijssen <nickthijssen1994@gmail.com>
2020-09-30 10:46:55 +0200

    Added Methods To GUI

--- animal-shelter/src/main/java/gui/Controller.java
+++ animal-shelter/src/main/java/gui/Controller.java
@@ -1,11 +1,26 @@
 package gui;
 
+import animals.Gender;
+import animals.Species;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 import webshop.Webshop;
 
+import java.time.LocalDate;
+
 public class Controller {
+
+    private String animalName;
+    private Gender animalGender;
+    private Species animalSpecies;
+    private LocalDate lastWalk;
+    private String badHabbit;
+    private String productName;
+    private double productPrice;
+    private String username;
+    private double totalPrice;
 
     @FXML
     private GridPane grid;
@@ -75,5 +90,65 @@ public class Controller {
     @FXML
     public void initialize() {
         webshop = new Webshop();
+    }
+
+    @FXML
+    void buttonAddAnimalClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void buttonAddProductClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void buttonAddToCartClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void buttonCheckoutClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void buttonRemoveFromCartClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void buttonReserveAnimalClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void buttonWalkDogClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void animalNameChanged(ActionEvent actionEvent) {
+        animalName = tfAnimalName.getText();
+    }
+
+    @FXML
+    void lastWalkDateChanged(ActionEvent actionEvent) {
+        lastWalk = dpLastWalk.getValue();
+    }
+
+    @FXML
+    void badHabbitChanged(ActionEvent actionEvent) {
+        badHabbit = tfBadHabbit.getText();
+    }
+
+    @FXML
+    void productNameChanged(ActionEvent actionEvent) {
+        productName = tfProductName.getText();
+    }
+
+    @FXML
+    void usernameChanged(ActionEvent actionEvent) {
+        username = tfUserName.getText();
     }
 }
